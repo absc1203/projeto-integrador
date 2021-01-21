a3e842bf515299068ac1a33038c2c4cccab9529f
barbisliboni <niinaliboni@gmail.com>
2021-01-21 14:58:46 -0300

    E commerce VIT

---
+++ VIT/VIT/src/main/java/com/vit/VIT/VitApplication.java
@@ -0,0 +1,13 @@
+package com.vit.VIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitApplication.class, args);
+	}
+
+}
---
+++ VIT/VIT/src/test/java/com/vit/VIT/VitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vit.VIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
