e37e646b6429e343eed480bd9ec6115407e8cf79
TiagointMartins <tiago.sanmart@gmail.com>
2021-03-03 09:29:42 -0300

    Alterações do projeto

--- frontEnd/VIT/src/app/app.module.ts
+++ frontEnd/VIT/src/app/app.module.ts
@@ -18,14 +18,11 @@ import { CamisetaComponent } from './camiseta/camiseta.component';
 import { ProdutosDiversosComponent } from './produtos-diversos/produtos-diversos.component';
 import { IndexComponent } from './index/index.component';
 import { HttpClientModule } from '@angular/common/http';
-<<<<<<< HEAD
 import { ProdutoIndividualComponent } from './produto-individual/produto-individual.component';
-=======
 import { ProdutosComponent } from './produtos/produtos.component';
 import { PagamentoComponent } from './pagamento/pagamento.component';
 import { TrocaComponent } from './troca/troca.component';
 import { PoliticaPrivacidadeComponent } from './politica-privacidade/politica-privacidade.component';
->>>>>>> 6c86e9ebb1fbb6bfdace8484757b6c76ee2d6739
 
 
 @NgModule({
@@ -43,13 +40,13 @@ import { PoliticaPrivacidadeComponent } from './politica-privacidade/politica-pr
     CamisetaComponent,
     ProdutosDiversosComponent,
     IndexComponent,
-<<<<<<< HEAD
-    ProdutoIndividualComponent
-=======
+    ProdutoIndividualComponent,
     ProdutosComponent,
     TrocaComponent,
-    PoliticaPrivacidadeComponent
->>>>>>> 6c86e9ebb1fbb6bfdace8484757b6c76ee2d6739
+    PoliticaPrivacidadeComponent,
+    PagamentoComponent
+
+
   ],
   imports: [
     BrowserModule,
