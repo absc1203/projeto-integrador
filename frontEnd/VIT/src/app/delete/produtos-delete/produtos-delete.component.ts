5413184dbcabff02d88384016733ef21b9b5a452
Isabel-Oyama <emiko.oyama93@gmail.com>
2021-03-03 14:04:23 -0300

    produto-delete atualizado

--- frontEnd/VIT/src/app/delete/produtos-delete/produtos-delete.component.ts
+++ frontEnd/VIT/src/app/delete/produtos-delete/produtos-delete.component.ts
@@ -1,4 +1,9 @@
+import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Produto } from 'src/app/model/Produto';
+import { ProdutoService } from 'src/app/service/produto.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-produtos-delete',
@@ -7,9 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutosDeleteComponent implements OnInit {
 
-  constructor() { }
+  produto: Produto = new Produto()
+  idProduto: number
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private produtoService: ProdutoService
+  ) { }
 
   ngOnInit(): void {
+
+    window.scroll(0,0)
+
+    if(environment.token == ''){
+      this.router.navigate(['index'])
+    }
+
+    this.idProduto = this.route.snapshot.params['id']
+    this.findByIdProduto(this.idProduto)
   }
 
+  findByIdProduto(id: number){
+    this.produtoService.getByIdProduto(id).subscribe((resp: Produto) => {
+      this.produto = resp
+    })
+  }
+
+  apagarProduto(){
+    this.produtoService.deleteProduto (this.idProduto). subscribe (() => {
+      alert('Produto apagado com sucesso!')
+      this.router.navigate(['cadastro-produto'])
+    })
+  }
 }
