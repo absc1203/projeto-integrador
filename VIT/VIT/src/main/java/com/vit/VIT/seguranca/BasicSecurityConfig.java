e06ed2eecb4cea2b3c6b27f5b05daf429a7f3dfd
barbisliboni <niinaliboni@gmail.com>
2021-03-16 12:33:41 -0300

    Alterando para dar o merge

--- VIT/VIT/src/main/java/com/vit/VIT/seguranca/BasicSecurityConfig.java
+++ VIT/VIT/src/main/java/com/vit/VIT/seguranca/BasicSecurityConfig.java
@@ -32,6 +32,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
 		http.authorizeRequests()
 		.antMatchers("/usuarios/logar").permitAll()
 		.antMatchers("/usuarios/cadastrar").permitAll()
+		.antMatchers("/**").permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
